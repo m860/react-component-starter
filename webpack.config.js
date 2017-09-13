b6bf602e552465101099517cb11a03a24a794fb3
jean <jean.ma.1986@gmail.com>
2017-09-13 17:43:34 +0800

    update webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -5,117 +5,103 @@ var CleanWebpackPlugin = require('clean-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var autoprefixer = require('autoprefixer');
 
-var isProduction = function () {
-	return process.env.NODE_ENV === 'production';
-};
-
-console.log("environment:%s", process.env.NODE_ENV);
-
-var output = {
-	path: path.join(__dirname, "dist")
-};
-if (isProduction()) {
-	output.filename = "[hash].js";
-	output.chunkFilename = "[chunkhash].[hash].js";
-	// output.chunkFilename = "[id].[hash].js";
-}
-else {
-	output.filename = "bundle.js";
-	output.chunkFilename = "[chunkhash].bundle.js";
-}
-
-var plugins = [
-	new webpack.optimize.CommonsChunkPlugin({
-		name:"vendor"
-		,filename:"vendor.bundle.js"
-	})
-	//clean dist
-	, new CleanWebpackPlugin(['dist'], {
-		root: __dirname,
-		verbose: true,
-		dry: false
-	})
-	//inject style & javascript to index.html template
-	, new HtmlWebpackPlugin({
-		filename: "index.html",
-		template: './src/index.html',
-		inject: false
-	})
-
-];
-if (isProduction()) {
-	//package style
-	plugins.push(new ExtractTextPlugin("[contenthash].css"));
-	//compress javascript
-	plugins.push(new webpack.optimize.UglifyJsPlugin({
-		compress: {
-			warnings: false
-		}
-	}));
-	plugins.push(new webpack.optimize.DedupePlugin());
-}
-else {
-	plugins.push(new ExtractTextPlugin("style.css"));
-}
-
-module.exports = {
-	entry: {
-		index: "./src/app.js"
+module.exports = function (env) {
+	if (!env) {
+		env = {};
 	}
-	, output: output
-	, module: {
-		loaders: [
-			{
+	if (!env.NODE_ENV) {
+		env.NODE_ENV = 'development';
+	}
+	console.log('env.NODE_ENV=' + env.NODE_ENV);
+	var isProduction = env.NODE_ENV !== 'development';
+	var plugins = [
+		new webpack.optimize.CommonsChunkPlugin({
+			name: "vendor"
+			, filename: "vendor.bundle.js"
+		})
+		//clean dist
+		, new CleanWebpackPlugin(['dist'], {
+			root: __dirname,
+			verbose: true,
+			dry: false
+		})
+		//inject style & javascript to index.html template
+		, new HtmlWebpackPlugin({
+			filename: "index.html",
+			template: './src/index.html',
+			inject: false
+		})
+		, new ExtractTextPlugin(isProduction ? "[contenthash].css" : "style.[contenthash].css")
+		, new webpack.LoaderOptionsPlugin({
+			options: {
+				postcss: [
+					autoprefixer({
+						browsers: ['> 5%']
+					})
+				]
+			}
+		})
+	];
+	if (isProduction) {
+		plugins.push(new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false
+			}
+		}));
+	}
+	var opt = {
+		entry: "./src/app.js"
+		, output: {
+			path: path.join(__dirname, "dist")
+			, filename: isProduction ? '[hash].js' : 'bundle.[hash].js'
+			, chunkFilename: isProduction ? '[chunkhash].[hash].js' : '[id].[chunkhash].[hash].js'
+		}
+		, module: {
+			rules: [{
 				test: /\.css$/,
-				loader: ExtractTextPlugin.extract("style-loader", "css-loader")
-			},
-			{
+				use: ExtractTextPlugin.extract({
+					fallback: "style-loader",
+					use: [{
+						loader: "css-loader"
+					}]
+				})
+			}, {
 				test: /\.(jsx|js)?$/
-				,exclude: /(node_modules|bower_components)/
-				// 'babel-loader' is also a legal name to reference
-				//loaders: ["babel-loader?presets[]=es2016,presets[]=react", "eslint-loader?{rules:{semi:0}}"]
-				, loader: "babel-loader"
+				, exclude: /(node_modules|bower_components)/
+				, use: "babel-loader"
 			}, {
 				test: /\.sass$/,
-				loader: ExtractTextPlugin.extract("style-loader", "css-loader!postcss-loader!sass-loader")
+				use: ExtractTextPlugin.extract({
+					fallback:"style-loader",
+					use:[
+						"css-loader",
+						"postcss-loader",
+						"sass-loader"
+					]
+				})
 			}, {
 				test: /\.(jpe?g|png|gif|svg)$/i,
-				loaders: [
+				use: [
 					'file?hash=sha512&digest=hex&name=[hash].[ext]',
 					'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
 				]
 			}, {
 				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-				loader: "url-loader?limit=10000&mimetype=application/font-woff"
-			},{
+				use: "url-loader?limit=10000&mimetype=application/font-woff"
+			}, {
 				test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-				loader: "file-loader"
-			},{
+				use: "file-loader"
+			}, {
 				test: /\.(ogg|mp3)$/,
-				loader: "file-loader"
-			}
-		]
-		, preLoaders: [
-			{
-				test: /\.jsx$/
-				, loader: "eslint-loader"
-				, exclude: /node_modules/
-			}
-		]
-	}
-	, babel: {
-		presets: ["es2015", 'stage-0', "react"]
-		, plugins: [
-			"transform-runtime"
-		]
-	}
-	, postcss: function () {
-		return [autoprefixer({browsers: ['> 5%']})];
-	}
-	, resolve: {
-		//设置别名
-		alias: {
+				use: "file-loader"
+			}]
 		}
-	}
-	, plugins: plugins
+		, resolve: {
+			//设置别名
+			alias: {}
+		}
+		, plugins: plugins
+	};
+
+	return opt;
 };
